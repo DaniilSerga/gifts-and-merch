c9f94b29ece83e361d2b4f676e058e75ef5eb2fa
DaniilSerga <daniil.serga@gmail.com>
2026-03-05 15:56:02 +0300

    init: initialize structure of the main frontend project, add libraries, eslint rules and plugins and routing

---
+++ frontend/src/app/routing/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { MainPage } from '@/pages';
+
+import { App } from '../App';
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [{ path: '/', element: <MainPage /> }],
+	},
+]);
